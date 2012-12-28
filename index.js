e2dfbd5f8260997c8d1bd7dd4ba05c5b578d3f25
Friedel Ziegelmayer <friedel.ziegelmayer@gmail.com>
2012-12-28 12:28:28 +0100

    Add rpc style communcation for console.log

--- index.js
+++ index.js
@@ -3,33 +3,61 @@ var engine = require("engine.io-stream");
 var $ = require('jquery-browserify');
 var MuxDemux = require('mux-demux');
 var TapProducer = require('./node_modules/tap/lib/tap-producer.js');
+var rpc = require('rpc-stream');
 
+
+// engine.io-stream as communication basis
 var stream = engine("/invert");
+
+
+// tap-producer to generate a stream of tap compliant data
 var tp = new TapProducer();
 
+// mux-demux to send the different streams over one connection
 var mdm = MuxDemux();
 
-var tapStream = null;
-var testacularStream = null;
 
+// Replace console.log with a remote call
+var rpcStream = rpc();
+
+var console = global.console = global.console || {log: function() {}};
+var browserConsoleLog = console.log;
+var remoteConsoleLog = rpcStream.wrap(['log']).log;
+
+console.log = function() {
+  var args = [].slice.call(arguments);
+  rpcStream.wrap('log').log(args, function() {
+    
+  });
+  browserConsoleLog.apply(console, args);
+};
+
+// Mix and mux all the streams
 mdm.on('connection', function(_stream) {
-  console.log('Connected', _stream.meta);
+  // print some dummy data send from the server
   _stream.pipe(mapping(function(chunk) {
     var element = String(chunk);
     $('#results_' + _stream.meta).append(element);
   }));
   
-  if (_stream.meta === 'tap') {
-      console.log(tp, _stream);
+  // entangle the streams
+  switch (_stream.meta) {
+    case 'tap':
       tp.pipe(_stream);
-    } else if (_stream.meta === 'testacular') {
-      testacularStream = _stream;
-    }
-  
+      break;
+    case 'testacular':
+      break;
+    case 'rpc':
+      _stream.pipe(rpcStream).pipe(_stream);
+      console.log('piped');
+      break;
+  }
 });
 
+// Hook mux-demux into engine.io-stream
 stream.pipe(mdm).pipe(stream);
 
+// Write some data to the tap stream
 setInterval(function() {
   tp.write({
     name: 'test',
@@ -37,7 +65,4 @@ setInterval(function() {
   });
 }, 1e3);
 
-// Disconnects
-stream.on('closed', function(reason, description) {
-  console.log('Disconnect!', reason, description);
-});
+
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var ecstatic = require('ecstatic');
 var MuxDemux = require('mux-demux');
 var TapConsumer = require('tap').Consumer;
 var EngineServer = require('engine.io-stream/server');
+var rpc = require('rpc-stream');
 
 var staticHandler = ecstatic(path.join(__dirname, 'static'));
 var server = http.createServer(staticHandler);
@@ -28,7 +29,14 @@ function onConnection(stream) {
     // });
     //tapStream.pipe(process.stdout, {end:false});
     var testacularStream = mdm.createStream('testacular');
-    
+    var rpcStream = mdm.createStream('rpc');
+    rpcFunctions = rpc({
+      log: function(args, callback) {
+        console.log.apply(console, args);
+        callback();
+      }
+    });
+    rpcStream.pipe(rpcFunctions).rpcStream;
     var iv = setInterval(function () {
         testacularStream.write(String(Math.floor(Math.random() * 4)));
     }, 1e3)
